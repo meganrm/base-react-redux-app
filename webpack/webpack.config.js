48bb69c6675359b5987fd860e5df85b9c0062f2d
Megan Riel-Mehan <meganrm@gmail.com>
2018-08-09 11:25:29 -0700

    setting up antd custom theme

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,15 +1,27 @@
 const path = require('path');
+const fs = require('fs');
 
+const lessToJs = require('less-vars-to-js');
+const themeVariables = lessToJs(fs.readFileSync(path.join(__dirname, '../src/styles/ant-vars.less'), 'utf8'));
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const tsImportPluginFactory = require('ts-import-plugin');
 
-const { Env } = require('./constants');
+const {
+    devServer,
+    Env,
+    stats,
+} = require('./constants');
 const getPluginsByEnv = require('./plugins');
 
 module.exports = ({ analyze, env } = {}) => ({
     devtool: env !== Env.PRODUCTION && 'source-map',
     devServer: {
-        port: 9002,
-        contentBase: path.join(__dirname, '../', 'dist')
+        contentBase: path.join(__dirname, '../', 'dist'),
+        disableHostCheck: true,
+        host: devServer.host,
+        port: devServer.port,
+        publicPath: `${devServer.host}:${devServer.port}:/${devServer.path}/`,
+        stats,
     },
     entry: {
         app: './src/index.tsx'
@@ -23,12 +35,33 @@ module.exports = ({ analyze, env } = {}) => ({
                 ],
                 exclude: /node_modules/,
                 use: {
-                    loader: 'awesome-typescript-loader',
+                    loader: 'ts-loader',
                     options: {
-                        configFileName: path.resolve(__dirname, '../', 'tsconfig.json')
-                    }
+                        configFile: path.resolve(__dirname, '../', 'tsconfig.json'),
+                        compilerOptions: {
+                            noEmit: false,
+                        },
+                        getCustomTransformers: () => ({
+                            before: [
+                                tsImportPluginFactory([
+                                    {
+                                        libraryName: 'lodash',
+                                        libraryDirectory: null,
+                                        camel2DashComponentName: false,
+                                        style: false,
+                                    }
+                                ]),
+                            ]
+                        }),
+                        // give responsibility of type checking to fork-ts-checker-webpack-plugin
+                        // in order to speed up build times
+                        transpileOnly: true,
+                    },
                 }
             },
+
+            // this rule processes any CSS written for this project and contained in src/
+            // it applies PostCSS plugins and converts it to CSS Modules
             {
                 test: /\.css/,
                 include: [
@@ -53,13 +86,60 @@ module.exports = ({ analyze, env } = {}) => ({
                                 plugins: [
                                     require('postcss-import'),
                                     require('postcss-cssnext')(),
-                                    require('postcss-responsive-font')
                                 ]
                             }
                         }
                     ]
                 })
-            }
+            },
+
+            // this rule will handle any css imports out of node_modules; it does not apply PostCSS,
+            // nor does it convert the imported css to CSS Modules
+            // e.g., importing antd component css
+            {
+                test: /\.css/,
+                include: [
+                    path.resolve(__dirname, '../', 'node_modules')
+                ],
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: [{ loader: 'css-loader' }],
+                }),
+            },
+            {
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                test: /\.js$/,
+                options: {
+                    plugins: [
+                        ['import', { libraryName: "antd", style: true }]
+                    ]
+                },
+            },
+            {
+                test: /\.less$/,
+                use: ExtractTextPlugin.extract({
+                    use: [
+                        // {loader: "style-loader"},
+                        {
+                            loader: "css-loader",
+                            options: {
+                                camelCase: true,
+                                importLoaders: 1
+                            }
+
+                        },
+                        {
+                            loader: "less-loader",
+                            options: {
+                                javascriptEnabled: true,
+                                modifyVars: themeVariables,
+
+                            }
+                        }
+                    ]
+                })
+            },
         ]
     },
     output: {
@@ -69,5 +149,6 @@ module.exports = ({ analyze, env } = {}) => ({
     plugins: getPluginsByEnv(env, analyze),
     resolve: {
         extensions: ['.ts', '.tsx', '.js', '.jsx', '.json']
-    }
+    },
+    stats: analyze ? "none" : stats,
 });
